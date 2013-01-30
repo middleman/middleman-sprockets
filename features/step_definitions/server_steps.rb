adeb1a7b63c91a280a3d8ce1720011b2c4959216
jordanandree <jordan.andree@mailchimp.com>
2013-01-30 11:20:31 -0500

    added response code step to server_steps

--- features/step_definitions/server_steps.rb
+++ features/step_definitions/server_steps.rb
@@ -2,6 +2,9 @@ Then /^I should see$/ do |contents|
   @browser.last_response.body.should include(contents)
 end
 
+Then /^I should get a response with status "(.*?)"$/ do |status|
+  @browser.last_response.status.should == status.to_i
+end
 Given /^wait a second$/ do
   sleep(1)
 end
