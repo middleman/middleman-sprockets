f160790922e1f1b28ea21dcc1c217c3d6a7430f7
jordanandree <jordan.andree@mailchimp.com>
2013-01-30 13:45:08 -0500

    cucumber tests for custom assets load paths

--- features/step_definitions/server_steps.rb
+++ features/step_definitions/server_steps.rb
@@ -1,3 +1,7 @@
 Then /^I should see$/ do |contents|
   @browser.last_response.body.should include(contents)
 end
+
+Then /^I should get a response with status "(.*?)"$/ do |status|
+  @browser.last_response.status.should == status.to_i
+end
