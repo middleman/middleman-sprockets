4ae36d0acf1ac61fa662d1d8a3ffae553d8e245c
jordanandree <jordan.andree@mailchimp.com>
2013-01-30 10:24:15 -0500

    have sprockets add images directory when looping through asset try_paths map images_dir to Middleman sprockets added response code step to server_steps cucumber test coverage for sprockets images

--- features/step_definitions/server_steps.rb
+++ features/step_definitions/server_steps.rb
@@ -2,6 +2,11 @@ Then /^I should see$/ do |contents|
   @browser.last_response.body.should include(contents)
 end
 
-Given /^wait a second$/ do
+Then /^I should get a response with status "(.*?)"$/ do |status|
+  @browser.last_response.status.should == status.to_i
+end
+Then /^I should get a response with status "(.*?)"$/ do |status|
+  @browser.last_response.status.should == status.to_i
+endGiven /^wait a second$/ do
   sleep(1)
 end
