4510214f4f1a9aa2420a90cc096bc01fbd8c3063
Ben Hollis <ben@benhollis.net>
2014-02-25 22:21:54 -0800

    Only patch Sass if Sass is present

--- lib/middleman-sprockets/sass_function_hack.rb
+++ lib/middleman-sprockets/sass_function_hack.rb
@@ -4,22 +4,24 @@
 #
 # See https://github.com/middleman/middleman/issues/864 for more info.
 #
-module Sass::Script::Functions
-  def image_url(source, options = {}, cache_buster = nil)
-    # Work with the Compass #image_url API
-    if options.respond_to? :value
-      case options.value
-      when true
-        return ::Sass::Script::String.new sprockets_context.image_path(source.value).to_s, :string
-      else
-        options = {}
+if defined?(Sass)
+  module Sass::Script::Functions
+    def image_url(source, options = {}, cache_buster = nil)
+      # Work with the Compass #image_url API
+      if options.respond_to? :value
+        case options.value
+        when true
+          return ::Sass::Script::String.new sprockets_context.image_path(source.value).to_s, :string
+        else
+          options = {}
+        end
       end
+      ::Sass::Script::String.new "url(\"#{sprockets_context.image_path(source.value, map_options(options)).to_s}\")"
     end
-    ::Sass::Script::String.new "url(\"#{sprockets_context.image_path(source.value, map_options(options)).to_s}\")"
-  end
 
-  # Also override generated_image_url to use Sprockets a la https://github.com/Compass/compass-rails/blob/98e4b115c8bb6395a1c3351926d574321396778b/lib/compass-rails/patches/3_1.rb
-  def generated_image_url(path, only_path = nil)
-    asset_url(path, Sass::Script::String.new("image"))
+    # Also override generated_image_url to use Sprockets a la https://github.com/Compass/compass-rails/blob/98e4b115c8bb6395a1c3351926d574321396778b/lib/compass-rails/patches/3_1.rb
+    def generated_image_url(path, only_path = nil)
+      asset_url(path, Sass::Script::String.new("image"))
+    end
   end
 end
