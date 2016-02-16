middleman/middleman-sprockets

commit 3815ccac9a9838af367d33370823a16f0f3cc4fa

refactor(asset paths): extract method for generating sprocket’s asset paths

lib/middleman-sprockets/extension.rb
@@ -48,7 +48,7 @@ module Middleman
           end
 
           if app.extensions[:sprockets].check_asset(path)
-            "/#{app.config[:sprockets_imported_asset_path]}/#{path}"
+            app.extensions[:sprockets].sprockets_asset_path( environment[path] )
           else
             app.asset_path(kind, path)
           end
@@ -69,7 +69,7 @@ module Middleman
     end
 
     def check_asset(path)
-      if asset = environment[path]
+      if environment[path]
         @inline_asset_references << path
         true
       else
@@ -95,17 +95,19 @@ module Middleman
         if sprockets_resource.respond_to?(:sprockets_asset) && !sprockets_resource.errored?
           sprockets_resource.sprockets_asset.links.each do |a|
             asset = environment[a]
-            path = "#{app.config[:sprockets_imported_asset_path]}/#{asset.logical_path}"
-            sum << generate_resource(path, asset.filename, asset.logical_path)
+            sum << generate_resource(sprockets_asset_path(asset), asset.filename, asset.logical_path)
           end
         end
 
         sum
       end + @inline_asset_references.map do |path|
         asset = environment[path]
-        path = "#{app.config[:sprockets_imported_asset_path]}/#{asset.logical_path}"  
-        generate_resource(path, asset.filename, asset.logical_path)
+        generate_resource(sprockets_asset_path(asset), asset.filename, asset.logical_path)
       end
+    end
+
+    def sprockets_asset_path sprockets_asset
+      File.join( app.config[:sprockets_imported_asset_path], sprockets_asset.logical_path)
     end
 
     private

commit d1e589c8d8e904376ac0f4e6c8eae22f48ce1503

include a leading ‘/‘ for asset paths — fixes regression introduced with 3815ccac9a9838af367d33370823a16f0f3cc4fa

lib/middleman-sprockets/extension.rb
@@ -18,6 +18,7 @@ module Middleman
     end
 
     def after_configuration
+
       @environment.append_path((app.source_dir + app.config[:js_dir]).to_s)
       @environment.append_path((app.source_dir + app.config[:css_dir]).to_s)
 
@@ -48,7 +49,7 @@ module Middleman
           end
 
           if app.extensions[:sprockets].check_asset(path)
-            app.extensions[:sprockets].sprockets_asset_path( environment[path] )
+            app.extensions[:sprockets].sprockets_asset_path( environment[path] ).sub(/^\/?/, '/')
           else
             app.asset_path(kind, path)
           end